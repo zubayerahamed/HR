bd8ee01e5189bca8f07877b7b7e8f74a832959eb
Zubayer Ahamed <zubayerahamed.freelancer@gmail.com>
2022-08-25 16:47:32 +0600

    d

--- src/main/java/com/kit/controller/FoodController.java
+++ src/main/java/com/kit/controller/FoodController.java
@@ -1,9 +1,12 @@
 package com.kit.controller;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +14,11 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -26,7 +29,9 @@ import com.kit.service.FoodService;
 import com.kit.service.UserService;
 import com.kit.util.Util;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author Zubayer Ahamed
@@ -36,22 +41,35 @@ import lombok.Data;
 @RequestMapping("/food")
 public class FoodController {
 
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
 	@Autowired private UserService userService;
 	@Autowired private FoodService foodService;
 
 	@GetMapping
 	public String loadPage(Model model) {
-		model.addAttribute("s", new SearchBody());
-		return "food";
+		return "redirect:/food/search/" + sdf.format(new Date());
 	}
 
 	@PostMapping("/search")
 	public String search(SearchBody s, Model model) {
 		model.addAttribute("s", s);
+		loadpage(s.getDate(), model);
+		return "food";
+	}
 
+	@GetMapping("/search/{date}")
+	public String search(@PathVariable String date, Model model) throws ParseException {
+		SearchBody s = new SearchBody(sdf.parse(date));
+		model.addAttribute("s", s);
+		loadpage(s.getDate(), model);
+		return "food";
+	}
+
+	private void loadpage(Date date, Model model) {
 		List<User> userList = userService.getAll();
-		List<Food> foodList = foodService.findByDate(s.getDate());
-		
+		List<Food> foodList = foodService.findByDate(date);
+
 		// TODO: filter user from joining date month
 
 		// merge food entry with users
@@ -71,17 +89,13 @@ public class FoodController {
 		}
 
 		model.addAttribute("foodList", finalFoodTable);
-
-		return "food";
 	}
 
 	@PostMapping(value = "/save", headers = "Accept=application/json")
-	public @ResponseBody String save(@RequestBody String json, Model model, RedirectAttributes ra) {
-
-		System.out.println(json);
+	public @ResponseBody Map<String, Object> save(@RequestBody String json) {
 		FoodAttendanceSheet c = new FoodAttendanceSheet();
 		ObjectMapper obm = new ObjectMapper();
-		obm.setDateFormat(new SimpleDateFormat("yyyy-MM-dd"));
+		obm.setDateFormat(sdf);
 		try {
 			JsonNode rootNode = obm.readTree(json);
 			JsonNode query = rootNode.get("fas");
@@ -100,7 +114,11 @@ public class FoodController {
 		}
 
 		foodService.save(foods);
-		return "food";
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("message", "Saved Successfully");
+		map.put("redirectUrl", "/food/search/" + sdf.format(c.getDate()));
+		return map;
 	}
 	
 }
@@ -113,6 +131,8 @@ class FoodAttendanceSheet{
 }
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 class SearchBody{
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date date;
